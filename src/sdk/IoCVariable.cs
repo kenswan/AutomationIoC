310b60d6af3ec2bf66d835549b9c3c122be1c0e4
Kenneth Swan <49496839+kenswan@users.noreply.github.com>
2022-04-04 08:30:53 -0500

    abstract set-variable

---
+++ src/sdk/IoCVariable.cs
@@ -0,0 +1,13 @@
+﻿using AutomationIoC.Runtime;
+using System.Management.Automation;
+
+namespace AutomationIoC.SDK
+{
+    public abstract class IoCVariable : PSCmdlet
+    {
+        protected void SetVariable(string key, object value)
+        {
+            AutomationIoCRuntime.SetEnvironment(SessionState, key, value);
+        }
+    }
+}
