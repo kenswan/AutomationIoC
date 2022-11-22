5d3fea11641791d059d951ddc3e295676e3ef096
Ken Swan <49496839+kenswan@users.noreply.github.com>
2022-11-22 16:32:28 -0600

    Use Hostbuilder vs. Manual Service Rollup (#22)

--- src/runtime/Context/ContextBuilder.cs
+++ src/runtime/Context/ContextBuilder.cs
@@ -1,7 +1,8 @@
 ﻿using AutomationIoC.Runtime.Dependency;
 using AutomationIoC.Runtime.Session;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AutomationIoC.Runtime.Context;
 
@@ -27,38 +28,52 @@ internal class ContextBuilder : IContextBuilder
     {
         startup.AutomationEnvironment = automationEnvironment;
 
-        var configurationBuilder = new ConfigurationBuilder();
+        IHost hostApplication = GenerateHostBuilder()
+            .ConfigureAppConfiguration(startup.Configure)
+            .ConfigureServices(services =>
+            {
+                startup.ConfigureServices(services);
 
-        startup.Configure(configurationBuilder);
+                RuntimeFactory.AddClientRuntime(services);
+            })
+            .Build();
 
-        startup.Configuration = configurationBuilder.Build();
-
-        var serviceCollection = new ServiceCollection();
-
-        serviceCollection.AddSingleton(startup.Configuration);
-
-        startup.ConfigureServices(serviceCollection);
-
-        RuntimeFactory.AddClientRuntime(serviceCollection);
-
-        sessionStorageProvider.StoreServiceProvider(serviceCollection.BuildServiceProvider());
+        sessionStorageProvider.StoreHostProvider(hostApplication);
     }
 
     public void BuildServices(IServiceCollection serviceCollection)
     {
-        RuntimeFactory.AddClientRuntime(serviceCollection);
+        IHost hostApplication = GenerateHostBuilder()
+            .ConfigureServices(services =>
+            {
+                foreach (ServiceDescriptor service in serviceCollection)
+                {
+                    services.Add(service);
+                }
 
-        sessionStorageProvider.StoreServiceProvider(serviceCollection.BuildServiceProvider());
+                RuntimeFactory.AddClientRuntime(services);
+            })
+            .Build();
+
+        sessionStorageProvider.StoreHostProvider(hostApplication);
     }
 
     public void InitializeCurrentInstance<TAttribute>(object instance) where TAttribute : Attribute
     {
-        var serviceProvider = sessionStorageProvider.GetCurrentServiceProvider();
-        using var scope = serviceProvider.CreateScope();
-        var dependencyBinder = scope.ServiceProvider.GetRequiredService<IDependencyBinder>();
+        IServiceProvider serviceProvider = sessionStorageProvider.GetCurrentServiceProvider();
+        using IServiceScope scope = serviceProvider.CreateScope();
+        IDependencyBinder dependencyBinder = scope.ServiceProvider.GetRequiredService<IDependencyBinder>();
 
         dependencyBinder.LoadFieldsByAttribute<TAttribute>(instance);
 
         dependencyBinder.LoadPropertiesByAttribute<TAttribute>(instance);
     }
+
+    private static IHostBuilder GenerateHostBuilder() =>
+        new HostBuilder()
+        .ConfigureLogging(loggingBuilder =>
+        {
+            loggingBuilder.AddDebug();
+            loggingBuilder.AddConsole();
+        });
 }
