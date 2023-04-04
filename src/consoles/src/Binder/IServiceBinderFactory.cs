22adecb41e2fa86d651217be3a5581d854f5276d
Ken Swan <49496839+kenswan@users.noreply.github.com>
2023-04-04 11:29:21 -0500

    #30 add initial console project

---
+++ src/consoles/src/Binder/IServiceBinderFactory.cs
@@ -0,0 +1,13 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Ken Swan All rights reserved.
+// Licensed under the MIT License
+// -------------------------------------------------------
+
+using System.CommandLine.Binding;
+
+namespace AutomationIoC.Consoles.Binder;
+
+internal interface IServiceBinderFactory
+{
+    public BinderBase<T> Create<T>();
+}
---
+++ src/consoles/test/UnitTest1.cs
@@ -0,0 +1,15 @@
+// -------------------------------------------------------
+// Copyright (c) Ken Swan All rights reserved.
+// Licensed under the MIT License
+// -------------------------------------------------------
+
+namespace AutomationIoC.Consoles.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
