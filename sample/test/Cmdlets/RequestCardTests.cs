1ee5e6906eb37b359a3980a81c0098af448440fb
Ken Swan <49496839+kenswan@users.noreply.github.com>
2023-05-31 10:12:15 -0500

    Adjust Sample project new package structure

--- sample/test/Cmdlets/RequestCardTests.cs
+++ sample/test/Cmdlets/RequestCardTests.cs
@@ -1,6 +1,11 @@
-﻿using AutomationIoC.Sample.Cmdlets;
-using AutomationIoC.Sample.Models;
+﻿// -------------------------------------------------------
+// Copyright (c) Ken Swan All rights reserved.
+// Licensed under the MIT License
+// -------------------------------------------------------
+
 using AutomationIoC.PSCmdlets.Tools;
+using AutomationIoC.Sample.Cmdlets;
+using AutomationIoC.Sample.Models;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -16,13 +21,13 @@ public class RequestCardTests
         var deckMock = new Mock<IDeck>();
         var expectedCard = new Card { Rank = Rank.Ace, Suit = Suit.Spade };
 
-        using var requestCardCommand =
+        using IAutomationCommand<RequestCard> requestCardCommand =
             AutomationSandbox.CreateContext<RequestCard, Startup>(services =>
                 services.AddTransient(_ => deckMock.Object));
 
         deckMock.Setup(deck => deck.Draw()).Returns(expectedCard);
 
-        var actualCard = requestCardCommand.RunCommand<Card>().First();
+        Card actualCard = requestCardCommand.RunCommand<Card>().First();
 
         actualCard.Should().BeEquivalentTo(expectedCard);
     }
