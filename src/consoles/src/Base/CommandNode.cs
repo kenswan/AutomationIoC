2a530834bbba82ad1f9a5450ca6a41a8d9ffc89a
Ken Swan <49496839+kenswan@users.noreply.github.com>
2023-05-31 10:56:48 -0500

    add command node to capture command chaining

---
+++ src/consoles/src/Base/CommandNode.cs
@@ -0,0 +1,40 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Ken Swan All rights reserved.
+// Licensed under the MIT License
+// -------------------------------------------------------
+
+namespace AutomationIoC.Consoles.Base;
+
+internal class CommandNode
+{
+    public string Name { get; private set; }
+
+    public IReadOnlyCollection<CommandNode> Commands => commands;
+
+    private readonly List<CommandNode> commands = new();
+
+    public CommandNode(string name)
+    {
+        Name = name;
+    }
+
+    public void AddCommand(CommandNode command) => commands.Add(command);
+
+    public bool NodeExists(string name)
+    {
+        foreach (CommandNode node in commands)
+        {
+            if (node.Name == name)
+            {
+                return true;
+            }
+
+            if (node.Commands.Any())
+            {
+                return node.NodeExists(name);
+            }
+        }
+
+        return false;
+    }
+}
