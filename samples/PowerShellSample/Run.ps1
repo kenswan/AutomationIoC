59bf1434b694aae7ee816eff979a196783801e75
Ken Swan <49496839+kenswan@users.noreply.github.com>
2024-11-27 21:01:33 -0600

    All upgraded dependencies for .NET 8/9

--- samples/PowerShellSample/Run.ps1
+++ samples/PowerShellSample/Run.ps1
@@ -1,14 +1,55 @@
 param(
-    [string]$header = "Default Header"
+    [Alias("h")]
+    [string]$header = "Default Header",
+
+    [Alias("c")]
+    [string] $buildConfiguration = "Release",
+
+    [Alias("r")]
+    [string] $runtime = "osx-arm64",
+
+    [switch] $clean,
+
+    [switch] $removeOnExit
 )
 
-Import-Module "${PSScriptRoot}/bin/Debug/net9.0/PowerShellSample.dll" -Verbose
+$outputDirectory = "${PSScriptRoot}/bin/${buildConfiguration}/net8.0/${runtime}/publish"
+$dllPath = "${outputDirectory}/PowerShellSample.dll"
+$moduleManifestPath = "${outputDirectory}/PowerShellSample.psd1"
+
+if ($clean) {
+    Write-Host "Cleaning Project" -ForegroundColor Cyan
+    dotnet clean --configuration $buildConfiguration --runtime $runtime
+}
+
+Write-Host "Building Project" -ForegroundColor Cyan
+dotnet publish --configuration $buildConfiguration --runtime $runtime --self-contained false
 
 try {
+    Import-Module -Name $moduleManifestPath -Verbose -Force
+
     Write-Report -h $header
 }
 catch {
     Write-Error "Error. Exception: $_"
 }
+finally {
 
-write-host "Script Complete"
+    if ($removeOnExit) {
+        Write-Host "Removing Module" -ForegroundColor Cyan
+
+        Remove-Module -Name $moduleManifestPath -ErrorAction SilentlyContinue
+
+        Write-Host "Releasing DLL" -ForegroundColor Cyan
+
+        if (Test-Path $dllPath) {
+            Remove-Item -Path $dllPath -Force
+            Write-Host "DLL Released" -ForegroundColor Green
+        }
+        else {
+            Write-Host "DLL not found for removal" -ForegroundColor Yellow
+        }
+    }
+}
+
+Write-host "Script Complete"
