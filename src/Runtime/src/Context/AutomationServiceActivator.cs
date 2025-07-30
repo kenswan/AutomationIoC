22c9a5851a58e23cf89fae5f5ee16104f6c666d8
Ken Swan <49496839+kenswan@users.noreply.github.com>
2025-07-30 10:25:58 -0500

    Add Automation Service Activator to Runtime

---
+++ src/Runtime/src/Context/AutomationServiceActivator.cs
@@ -0,0 +1,54 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Ken Swan. All rights reserved.
+// Licensed under the MIT License
+// -------------------------------------------------------
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AutomationIoC.Runtime.Context;
+
+internal class AutomationServiceActivator
+{
+    private string[] args = [];
+    private Action<HostBuilderContext, IConfigurationBuilder> buildConfiguration = (context, builder) => { };
+    private Action<HostBuilderContext, IServiceCollection> buildServices = (context, services) => { };
+    private IDictionary<string, string> configurationMapping = new Dictionary<string, string>();
+
+    private IHost? host;
+
+    internal IServiceProvider GetServiceProvider()
+    {
+        host ??= AutomationRuntime.GenerateRuntimeHost(
+            buildConfiguration: buildConfiguration,
+            buildServices: buildServices,
+            parameters: args,
+            parameterConfigurationMappings: configurationMapping);
+        return host.Services;
+    }
+
+    public void SetArgs(string[] args)
+    {
+        host = null;
+        this.args = args;
+    }
+
+    public void SetConfiguration(Action<HostBuilderContext, IConfigurationBuilder> buildConfiguration)
+    {
+        host = null;
+        this.buildConfiguration = buildConfiguration;
+    }
+
+    public void SetServices(Action<HostBuilderContext, IServiceCollection> buildServices)
+    {
+        host = null;
+        this.buildServices = buildServices;
+    }
+
+    public void SetConfigurationMapping(IDictionary<string, string> configurationMapping)
+    {
+        host = null;
+        this.configurationMapping = configurationMapping;
+    }
+}
---
+++ src/Runtime/test/Context/AutomationServiceActivatorTests.cs
@@ -0,0 +1,106 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Ken Swan. All rights reserved.
+// Licensed under the MIT License
+// -------------------------------------------------------
+
+using AutomationIoC.Runtime.Context;
+using AutomationIoC.Runtime.Test.TestBed.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutomationIoC.Runtime.Test.Context;
+
+public class AutomationServiceActivatorTests
+{
+    [Fact]
+    public void GetServiceProvider_ShouldConfigureServices()
+    {
+        // Arrange
+        const string configurationKey = "TestConfiguration";
+        const string configurationValue = "ExecutedConfiguration";
+        const string expectedFieldOneValue = "ExecutedConfiguration";
+        const string expectedFieldTwoValue = "ExecutedConfiguration2";
+
+        var activator = new AutomationServiceActivator();
+
+        activator.SetConfiguration((context, configurationBuilder) =>
+        {
+            var appSettings = new Dictionary<string, string>
+            {
+                {
+                    configurationKey, configurationValue
+                }
+            };
+            configurationBuilder.AddInMemoryCollection(appSettings);
+        });
+
+        activator.SetServices((context, services) =>
+        {
+            services.Configure<TestConfiguredHostServiceOptions>(configureOptions =>
+            {
+                configureOptions.FieldOne = expectedFieldOneValue;
+                configureOptions.FieldTwo = expectedFieldTwoValue;
+            });
+
+            services.AddScoped<TestConfiguredHostService>();
+        });
+
+        // Act
+        IServiceProvider? serviceProvider = activator.GetServiceProvider();
+
+        // Assert
+        Assert.NotNull(serviceProvider);
+
+        TestConfiguredHostService? testService =
+            serviceProvider.GetService<TestConfiguredHostService>();
+
+        Assert.NotNull(testService);
+        Assert.True(testService.GetKeyValue(configurationKey, out string? value));
+        Assert.Equal(configurationValue, value);
+        Assert.Equal(expectedFieldOneValue, testService.FieldOne);
+        Assert.Equal(expectedFieldTwoValue, testService.FieldTwo);
+    }
+
+    [Fact]
+    public void GetServiceProvider_ShouldReadConfigurationValuesFromArguments()
+    {
+        // Arrange
+        const string fieldOneKey = "TestConfiguredHostServiceOptions:FieldOne";
+        const string expectedFieldOneValue = "TestConfigurationValueOne";
+        const string fieldTwoKey = "TestConfiguredHostServiceOptions:FieldTwo";
+        const string expectedFieldTwoValue = "TestConfigurationValueTwo";
+
+        var activator = new AutomationServiceActivator();
+
+        activator.SetServices((context, services) =>
+        {
+            services.AddScoped<TestConfiguredHostService>();
+
+            services.AddOptions<TestConfiguredHostServiceOptions>()
+                .BindConfiguration(nameof(TestConfiguredHostServiceOptions));
+        });
+
+        activator.SetConfigurationMapping(new Dictionary<string, string>
+        {
+            ["--fieldOne"] = fieldOneKey,
+            ["--fieldTwo"] = fieldTwoKey
+        });
+
+        activator.SetArgs(["--fieldOne", expectedFieldOneValue, "--fieldTwo", expectedFieldTwoValue]);
+
+        // Act
+        IServiceProvider? serviceProvider = activator.GetServiceProvider();
+
+        // Assert
+        Assert.NotNull(serviceProvider);
+
+        TestConfiguredHostService? testService =
+            serviceProvider.GetService<TestConfiguredHostService>();
+
+        Assert.NotNull(testService);
+        Assert.True(testService.GetKeyValue(fieldOneKey, out string? actualFieldOneValue));
+        Assert.Equal(expectedFieldOneValue, actualFieldOneValue);
+        Assert.True(testService.GetKeyValue(fieldTwoKey, out string? actualFieldTwoValue));
+        Assert.Equal(expectedFieldTwoValue, actualFieldTwoValue);
+    }
+}
